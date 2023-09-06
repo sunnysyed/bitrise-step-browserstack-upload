a9467bc2408165e5ef099621e68f60a6e8c2f589
Sunny <sunnysyed93+github@gmail.com>
2023-09-06 11:26:54 -0500

    Update step.sh

--- step.sh
+++ step.sh
@@ -2,6 +2,6 @@
 set -e
 set -x
 set -o pipefail
-curl -u "$browserstack_username:$browserstack_access_key" -X POST https://api-cloud.browserstack.com/app-live/upload -F "file=@$upload_path" -F 'data={"custom_id": "'$custom_id'"}' | jq -j '.app_url' | cut -c6- |  envman add --key BS_APP_HASH_ID
-envman add --key BS_URL --value "https://app-live.browserstack.com/dashboard#os=android&os_version=12.0&device=Samsung+Galaxy+S22&app_hashed_id=$BS_APP_HASH_ID&scale_to_fit=false&speed=1&start=true"
+curl -u "$bs_username:$bs_access_key" -X POST https://api-cloud.browserstack.com/app-live/upload -F "file=@$upload_path" -F 'data={"custom_id": "'$custom_id'"}' | jq -j '.app_url' | cut -c6- |  envman add --key BS_APP_HASH_ID
+envman add --key BS_APP_URL --value "https://app-live.browserstack.com/dashboard#os=android&os_version=12.0&device=Samsung+Galaxy+S22&app_hashed_id=$BS_APP_HASH_ID&scale_to_fit=false&speed=1&start=true"
 
